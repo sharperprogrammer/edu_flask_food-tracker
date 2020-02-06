7482feae813f391420f6c5af54d477fee44a5723
sharperprogrammer <sharperprogrammer@gmail.com>
2020-02-06 10:31:29 -0500

    Initial commit. Routes go to templates. DB helper functions created. Database created.

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, g
+import sqlite3
+
+app = Flask(__name__)
+
+def connect_db():
+    sql = sqlite3.connect('food_log.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+@app.route('/food')
+def food():
+    return render_template('add_food.html')
+
+if __name__ == '__main__':
+    app.run()
